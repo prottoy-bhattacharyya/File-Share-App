20e734c61cfa35ee96f655b726fcf7a2e8e95d57
Prottoy Vhattacharyya <prottoybhattacharya@gmail.com>
2026-03-06 04:27:32 +0600

    direct transfer added

--- app/app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -15,35 +15,47 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
-    Button send_buton, receive_buton;
+    Button send_buton, receive_buton, direct_transfer_btn;
     ImageButton logout_btn;
     TextView username;
     LinearLayout user_info_btn;
+    UserLocalStore userLocalStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
+        
         RequestPermissions();
+        init_works();
+        exqListener();
+    }
 
-        UserLocalStore userLocalStore = new UserLocalStore(this);
+    public void init_works() {
+
 
         username = findViewById(R.id.text_username);
         send_buton = findViewById(R.id.send_btn);
+        direct_transfer_btn = findViewById(R.id.direct_transfer_btn);
         receive_buton = findViewById(R.id.receive_btn);
         logout_btn = findViewById(R.id.button_logout);
         user_info_btn = findViewById(R.id.user_info_btn);
+
+        userLocalStore = new UserLocalStore(MainActivity.this);
 
         username.setText(userLocalStore.getUsername());
 
         if(!userLocalStore.isLoggedIn()){
             username.setText(getResources().getString(R.string.guest));
         }
+    }
 
-
+    public void exqListener() {
         user_info_btn.setOnClickListener(view -> {
             if(!userLocalStore.isLoggedIn()){
                 Intent intent = new Intent(MainActivity.this, loginActivity.class);
@@ -72,24 +84,34 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(MainActivity.this, receiveActivity.class);
             startActivity(intent);
         });
-
-
+        direct_transfer_btn.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, directTransferActivity.class);
+            startActivity(intent);
+        });
     }
 
-    public void RequestPermissions() {
+    private void RequestPermissions() {
+        List<String> permissions = new ArrayList<>();
+        permissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            ActivityCompat.requestPermissions(this, new String[]{
-                    Manifest.permission.READ_EXTERNAL_STORAGE,
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                    Manifest.permission.ACCESS_FINE_LOCATION,
-                    Manifest.permission.NEARBY_WIFI_DEVICES
-            }, 1);
+            permissions.add(Manifest.permission.NEARBY_WIFI_DEVICES);
+            permissions.add(Manifest.permission.READ_MEDIA_IMAGES);
+            permissions.add(Manifest.permission.READ_MEDIA_VIDEO);
+            permissions.add(Manifest.permission.READ_MEDIA_AUDIO);
         } else {
-            ActivityCompat.requestPermissions(this, new String[]{
-                    Manifest.permission.READ_EXTERNAL_STORAGE,
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                    Manifest.permission.ACCESS_FINE_LOCATION
-            }, 1);
+            permissions.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+            permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+
+        List<String> listPermissionsNeeded = new ArrayList<>();
+        for (String p : permissions) {
+            if (ActivityCompat.checkSelfPermission(this, p) != PackageManager.PERMISSION_GRANTED) {
+                listPermissionsNeeded.add(p);
+            }
+        }
+
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(this, listPermissionsNeeded.toArray(new String[0]), 100);
         }
     }
 }
